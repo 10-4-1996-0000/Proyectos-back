291e50b5c821bd7a1ca76faf5ca12e892f4a1d1d
Marcelo <129922139+10-4-1996-0000@users.noreply.github.com>
2023-05-13 19:25:56 -0300

    Add files via upload

---
+++ CalculadoraIng/src/CalculadoraIng/CalculadoraIng.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculadoraIng;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class CalculadoraIng {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Welcome, Insert number 1");
+        int num1 = leer.nextInt();
+        System.out.println("Insert number 2");
+        int num2 =leer.nextInt();
+        System.out.println("Insert option for validation: \n"
+                + "1-adittion\n"
+                + "2-rest\n"
+                + "3-multiplication\n"
+                + "4-divition\n"
+                + "5-exit");
+        int option = 0;
+        while (option!=5) {
+            switch(option){
+                case 1:
+            int adittion = num1 + num2;
+                case 2:
+                    int 
+            }
+            Object nextElement = en.nextElement();
+            
+        }
+    }
+    
+}
